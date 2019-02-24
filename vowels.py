0a11e5d9d09c78048867ea2f54fcfd51d63f7bc7
jowy3 <m>
2019-02-25 01:16:44 +0300

    my vowels

--- vowels.py
+++ vowels.py
@@ -1,3 +1,8 @@
-list_vowels = ['a','e','i']
-list_vowels += ['o', 'u',]
-print(list_vowels)
+def countvowels(strings):
+   vowel = "aeiou"
+   list_vowels = 0
+   print ([letter for letter in strings if letter in vowel])
+   print (len([letter for letter in strings if letter in vowel]))
+countvowels('drink water')
+
+
